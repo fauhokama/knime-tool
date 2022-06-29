4d7b0502e802ad569eef4efc271d5949730fd581
Faustino Hokama <fhokama@equoplatform.com>
2022-06-28 22:53:07 -0300

    Create download folder if it does not exist

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { override } from "prompts";
 import initial from "./commands/initial";
+import { existsSync, mkdirSync } from "fs"
+import { DOWNLOAD_FOLDER } from "./constants";
 import { parseArgs } from "./util/parseArgs";
 
 const main = async (args: string[]) => {
+	if (!existsSync(DOWNLOAD_FOLDER)) { mkdirSync(DOWNLOAD_FOLDER); }
 	override(parseArgs(args));
 	await initial.action(await initial.question());
 };
