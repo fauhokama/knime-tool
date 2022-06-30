0f5bc6acf14879eb79d08d7eec9177ed62839e20
Faustino Hokama <fhokama@equoplatform.com>
2022-06-30 01:43:12 -0300

    Add headers to downloader

--- src/util/download.ts
+++ src/util/download.ts
@@ -6,14 +6,17 @@ export default async (url: string, directory: string): Promise<string | undefine
 		url,
 		directory,
 		maxAttempts: 3,
-		onProgress: (percentage: string) => {
+		onProgress: (percentage: string, _: string, remainingSize: string) => {
 			process.stdout.clearLine(0);
 			process.stdout.cursorTo(0);
-			process.stdout.write(percentage);
+			process.stdout.write(`${percentage}%	Remaining bytes:${remainingSize}`);
 		},
 		skipExistingFileName: true,
 		onBeforeSave: (deducedName: string) => {
 			filename = deducedName;
+		},
+		headers: {
+			"User-Agent": "KNIME Tool",
 		},
 	});
 	try {
