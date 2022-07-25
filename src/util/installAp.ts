4c93455ca4a3b02c99d06b6228582c7f8020c55c
Faustino Hokama <fhokama@equoplatform.com>
2022-07-24 21:39:00 -0300

    Minor improvement

--- src/util/installAp.ts
+++ src/util/installAp.ts
@@ -1,9 +1,10 @@
-import { bold, green } from "kleur";
+import { bold, cyan, green } from "kleur";
 import extension from "../commands/subcommands/extension";
 import knimeIni from "../commands/subcommands/knimeIni";
-import { REPOSITORIES } from "../constants";
+import { DOWNLOAD_FOLDER, REPOSITORIES } from "../constants";
 import { getAbsolutePath } from "./ap";
-import { downloadAndDecompress } from "./downloadAndDecompress";
+import decompress from "./common/decompress";
+import download from "./common/download";
 
 // Downloads, Decompress and install extensions & knimeIni.
 // Returns absolutePath to AP dir.
@@ -15,4 +16,12 @@ export const installAp = async (url: string, extensions: string[], knimeini: str
     console.log(green(`Open AP by running the ${bold("list")} command`));
 
     return absolutePath;
+}
+
+const downloadAndDecompress = async (url: string) => {
+    console.log(`${cyan(`Downloading from URL:`)} ${url}`);
+    const filename = await download(url, DOWNLOAD_FOLDER);
+    const source = getAbsolutePath(filename as string);
+    console.log(`${cyan(`Decompressing...`)}`);
+    return decompress(source, DOWNLOAD_FOLDER, true);
 }
